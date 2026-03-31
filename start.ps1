b28e5ce6fbec6e6b92dc9f9c41212e639d916294
bebyx <bebyx@protonmail.com>
2026-04-01 00:57:26 +0300

    Add producer deployment

--- start.ps1
+++ start.ps1
@@ -2,3 +2,14 @@
 minikube start --driver=docker
 
 kubectl cluster-info --context minikube
+
+cd terraform
+terraform init
+terraform apply -auto-approve
+cd ..
+
+kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-single-node.yaml -n kafka
+
+docker build -t postgres-producer:local ./src/producer
+minikube image load postgres-producer:local
+kubectl apply -f k8s/producer-deployment.yaml
